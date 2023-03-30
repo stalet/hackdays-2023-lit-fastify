bd95805ffd14d6d545faf02f7d624083c4a2b425
Ståle Tomten <stale.tomten@finn.no>
2023-03-30 11:12:35 +0200

    Linting.

--- src/rick-morty-component.ts
+++ src/rick-morty-component.ts
@@ -1,16 +1,15 @@
 import { LitElement, html } from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 
 @customElement('rick-morty')
 export class RickMortyComponent extends LitElement {
+    @property()
+    version = 'STARTING';
 
-  @property()
-  version = 'STARTING';
-
-  render() {
-    return html`
-      <h1>Hello, world!</h1>
-      <p>This is a web component with Lit inside. ${this.version}</p>
-    `;
-  }
+    render() {
+        return html`
+            <h1>Hello, world!</h1>
+            <p>This is a web component with Lit inside. ${this.version}</p>
+        `;
+    }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,46 @@
-import {html} from 'lit';
+import { html } from 'lit';
 import fastify from 'fastify';
 import fastifyStatic from '@fastify/static';
-import {join} from 'path';
+import { join } from 'path';
 import * as url from 'url';
-import {TemplateResult} from "lit-html";
+import { TemplateResult } from 'lit-html';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const app = fastify();
 
 app.register(fastifyStatic, {
-  root: join(__dirname, '../eik'),
-  prefix: "/public/",
-  index: false,
-  list: true
+    root: join(__dirname, '../eik'),
+    prefix: '/public/',
+    index: false,
+    list: true,
 });
 
 const getRenderString = (data: TemplateResult) => {
-    const {strings, values} = data;
-    const v = [...values, '']
-    return strings.reduce((acc,s, i) => acc + s + v[i], '')
-}
+    const { strings, values } = data;
+    const v = [...values, ''];
+    return strings.reduce((acc, s, i) => acc + s + v[i], '');
+};
 
-const template = (content: unknown) => html`
-<html lang="en">
-  <head>
-    <title>Rick And Morty Web Component</title>
-  </head>
-  <body>
-    <h1>Rick and morty web component!</h1>
-    ${content}
-    <script type="module" src="/public/esm.js"></script>
-  </body>
+const template = (content: unknown) => html` <html lang="en">
+    <head>
+        <title>Rick And Morty Web Component</title>
+    </head>
+    <body>
+        <h1>Rick and morty web component!</h1>
+        ${content}
+        <script type="module" src="/public/esm.js"></script>
+    </body>
 </html>`;
 
 app.get('/', (request, reply) => {
-    reply.type('text/html').send(getRenderString(template((`<rick-morty></rick-morty>`))))
-})
+    reply
+        .type('text/html')
+        .send(getRenderString(template(`<rick-morty></rick-morty>`)));
+});
 
 app.listen({ port: 3000 }, (err, address) => {
     if (err) {
-        console.error(err)
-        process.exit(1)
+        console.error(err);
+        process.exit(1);
     }
 });
